c15d77f6a6454598ff812234edeabe3fffd60c44
MiraWeng <1965510789@qq.com>
2019-06-07 17:12:29 +0800

    侧边栏和toobar以及登录拦截完成

--- public/back/js/common.js
+++ public/back/js/common.js
@@ -1,3 +1,5 @@
+
+// 进度条
 $(document).ajaxStart(function(){
     NProgress.start();
 });
@@ -6,4 +8,46 @@ $(document).ajaxStop(function(){
     setTimeout(function(){
         NProgress.done();
     },500);
+});
+
+//登录拦截
+if(location.href.indexOf("login.html")===-1){
+    $.ajax({
+        type:"get",
+        url:"/employee/checkRootLogin",
+        dataType:"json",
+        success:function(info){
+            if(info.success){
+                console.log(info);
+            }
+            if(info.error===400){
+                location.href="login.html";
+            }            
+        }
+    });
+}
+
+// 顶部菜单栏切换显示功能
+$(".lt-topbar .menu").click(function(){
+    $(".lt-aside").toggleClass("hidemenu");
+    $(".lt-main").toggleClass("hidemenu");
+    $(".lt-topbar").toggleClass("hidemenu");
+});
+//点击退出显示模态框
+$(".lt-topbar .logout").click(function(){
+    $('#myModal').modal('show');
+});
+
+// 点退出按钮退出
+$(".logoutBtn").click(function(){
+    $.ajax({
+        url:"/employee/employeeLogout",
+        type:"get",
+        dataType:"json",
+        success:function(info){
+            if(info.success){
+                location.href="login.html";
+            }
+        }
+    });
 });
